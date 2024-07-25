b60299ef69ebe43e265e8e52acdf2085bd8ebf42
Ganesan Chandran <ganesanchandran@hotmail.com>
2024-07-25 12:10:24 +0530

    fix: db file name fixed

--- src/utils/db/dbPaths.ts
+++ src/utils/db/dbPaths.ts
@@ -2,16 +2,16 @@ import path from "path";
 import { getExtDbPath } from "./getExtDbPath";
 
 export const cookieDBPath = () =>
-  path.resolve(getExtDbPath(), "fetchClientCookies.json");
+  path.resolve(getExtDbPath(), "fetchClientCookies.db");
 
 export const historyDBPath = () =>
-  path.resolve(getExtDbPath(), "fetchClientHistory.json");
+  path.resolve(getExtDbPath(), "fetchClientHistory.db");
 
 export const collectionDBPath = () =>
-  path.resolve(getExtDbPath(), "fetchClientCollection.json");
+  path.resolve(getExtDbPath(), "fetchClientCollection.db");
 
 export const mainDBPath = () =>
-  path.resolve(getExtDbPath(), "fetchClient.json");
+  path.resolve(getExtDbPath(), "fetchClient.db");
 
 export const variableDBPath = () =>
-  path.resolve(getExtDbPath(), "fetchClientVariable.json");
+  path.resolve(getExtDbPath(), "fetchClientVariable.db");
